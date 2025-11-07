450b918260568d692d2a42258b6ba3a99114ca84
Hugo Santini <hugosantini26@gmail.com>
2025-11-07 12:38:14 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-Loja ebac/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe ('Funcionalidade: Login',() => {
+
+    it ('Deve fazer login com sucesso', () =>{
+        cy.visit( 'http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type ('hugosantini92@gmail.com')
+        cy.get('#password').type ('skateboard1')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, hugo santini (não é hugo santini? Sair)')
+
+    })
+})
