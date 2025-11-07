b3cff38d0ad793510d95fbb4d505ad837819e1ff
Hugo Santini <hugosantini26@gmail.com>
2025-11-07 13:25:48 -0300

    Atualiza teste de produtos e corrige seleção de cor

--- cypress/e2e/3-Loja ebac/produtos.cy.js
+++ cypress/e2e/3-Loja ebac/produtos.cy.js
@@ -1,30 +1,26 @@
-// cypress/e2e/produtos.cy.js
-describe('Produtos - lojaebac', () => {
-  const baseUrl = 'http://lojaebac.ebaconline.art.br';
+describe('Produtos - Loja EBAC', () => {
 
-  it('buscar produto e adicionar ao carrinho', () => {
-    cy.visit(baseUrl);
-    // Exemplo: abrir página de um produto direto (ajuste o slug)
-    cy.visit(baseUrl + '/product/eos-v-neck-hoodie/'); // ajuste conforme slug do produto
+    it('Deve selecionar um produto, escolher opções e adicionar ao carrinho', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/')
 
-    // Seletores para adicionar ao carrinho (ajuste conforme o botão)
-    cy.get('button.single_add_to_cart_button, button.add_to_cart, form.cart button[type="submit"]').click();
+        // Clica no primeiro produto da lista
+        cy.get('.product-block').first().click()
 
-    // Verificar mensagem de adicionado
-    cy.contains(/adicionado|adicionado ao seu carrinho|added to your cart/i, { timeout: 10000 }).should('exist');
+        // Seleciona tamanho
+        cy.get('.button-variable-item-M').click()
 
-    // Ir para o carrinho
-    cy.visit(baseUrl + '/cart/');
-    // Verificar se produto está no carrinho
-    cy.get('.cart_table, table.shop_table').should('exist');
-    cy.contains('Eos V-Neck Hoodie', { timeout: 5000 }).should('exist'); // ajuste título do produto conforme o site
-  });
+        // Seleciona cor existente no produto
+        cy.get('.button-variable-item-White').click()
 
-  it('fluxo rápido de checkout exige login/dados', () => {
-    cy.visit(baseUrl + '/cart/');
-    cy.get('a.checkout-button, .checkout-button, a[href*="/checkout"]').click();
-    // Deve exigir dados de pagamento ou login - verificar mensagem/URL
-    cy.url().should('include', '/checkout');
-    cy.contains(/payment|pagamento|endereço|checar/i).should('exist');
-  });
-});
+        // Adiciona ao carrinho
+        cy.get('.single_add_to_cart_button').click()
+
+        // Valida mensagem de sucesso
+        cy.get('.woocommerce-message')
+            .should('contain', 'foi adicionado no seu carrinho')
+
+        // Abre o carrinho
+        cy.visit('http://lojaebac.ebaconline.art.br/carrinho/')
+    })
+
+})
